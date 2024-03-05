75c711d9ea5c8b62e37824925b5fbdd5d3e69f71
Dheerajreddy2109 <reddydheeraj2109@gmail.com>
2024-03-06 01:03:07 +0530

    js and css shoted

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const header = document.querySelector("header");
 const menu = document.querySelector("#menu-btn")
 const close = document.querySelector("#close-btn")
 const modeText = document.querySelector("#mode-text")
+// let toggleSlider = document.querySelector("#toggleSlider");
 let headerBG = "light"
 
 
@@ -17,6 +18,37 @@ window.addEventListener("scroll",()=>{
 
 
 
+// const forAllLocal = ()=>{
+//     body.classList.toggle('dark-mode');
+//     if(headerBG === "light"){
+//         headerBG = "dark"
+//         header.style.backgroundColor = "#333138";
+//         // header.style.transition = "1s";
+//         header.style.color = "white";
+//         menu.style.color = "white";
+//         close.style.color = "white";
+//         modeText.innerText = "Light Mode"
+//         localStorage.setItem("enabeledDark",'true');
+      
+//     }
+//     else{
+//         headerBG = "light"
+//         header.style.backgroundColor = "rgb(222, 222, 222)";
+//         header.style.color = "black"
+//         // header.style.transition = "1s"
+//         menu.style.color = "black";
+//         close.style.color = "black";
+//         modeText.innerText = "Dark Mode"
+//         localStorage.removeItem("enabeledDark");
+
+
+
+//     }
+
+// };
+
+
+
 const forAllLocal = ()=>{
     body.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
@@ -24,11 +56,15 @@ const forAllLocal = ()=>{
     close.classList.toggle('dark-mode');
     if(headerBG === "light"){
         headerBG = 'dark'
+        header.style.backgroundColor = "#333138";
+        header.style.color = "white";
         modeText.innerText = "Light Mode"
         localStorage.setItem("enabeledDark",'true');
     }
     else{
         headerBG = "light"
+        header.style.backgroundColor = "rgb(222, 222, 222)";
+        header.style.color = "black"
         modeText.innerText = "Dark Mode"
         localStorage.removeItem("enabeledDark")
         
